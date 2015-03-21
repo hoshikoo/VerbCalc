4adf78e8c586771d0e97aecbbcb23a8350dde18e
hoshikoo <hoshikooki@gmail.com>
2015-03-21 13:29:48 -0400

    fixed method

--- src/nyc/c4q/hoshikoo/VerbosityCalculator.java
+++ src/nyc/c4q/hoshikoo/VerbosityCalculator.java
@@ -15,19 +15,24 @@ public class VerbosityCalculator {
         File aTaleOfTwoCities = new File("/Users/Hoshiko/Desktop/accesscode/VerbCalc/resources/A_Tale_of_Two_Cities.txt");
 
 
-        System.out.println("Hermal Melville's Moby Dick verbosity is ");
-        VerbosityCal(mobyDick);
-        System.out.println("Charles Dickens' A Tale of Two Cities verbosity is ");
-        VerbosityCal(aTaleOfTwoCities);
+        //System.out.println("Hermal Melville's Moby Dick verbosity is ");
+        //VerbosityCal(mobyDick);
+        //System.out.println("Charles Dickens' A Tale of Two Cities verbosity is ");
+       // VerbosityCal(aTaleOfTwoCities);
+        DecimalFormat df = new DecimalFormat("###,##0.00");
+        System.out.println(df.format(VerbosityCal(mobyDick)));
 
         if(VerbosityCal(mobyDick)>VerbosityCal(aTaleOfTwoCities)){
             System.out.println("Hermal Melville is more verbose than Charles Dickens");
         }else if (VerbosityCal(mobyDick)<VerbosityCal(aTaleOfTwoCities)){
             System.out.println("Charles Dickens is more verbose than Hermal Melville");
         }
+
     }
 
-    public static void VerbosityCal(File file) {
+    public static double VerbosityCal(File file) {
+
+        double verbosity=0.0;
         try {
 
             Scanner sc = new Scanner(file);
@@ -42,31 +47,23 @@ public class VerbosityCalculator {
                 //System.out.println(sc.next());
                 if (words.contains(".") || words.contains("!") || words.contains("?"))
                     sentences=sentences+1;
-<<<<<<< HEAD
 
-=======
-                
->>>>>>> 6d618da331ac011388fba7804d172b52f6c309f4
                 if (words.equalsIgnoreCase("mr.") || words.equalsIgnoreCase("mrs.") || words.equalsIgnoreCase("ms."))
                     sentences=sentences-1;
 
             }
-            double verbosity = wordCount/sentences;
+            verbosity = wordCount/sentences;
             //System.out.println(wordCount);
             //System.out.println(sentences);
-            DecimalFormat df = new DecimalFormat("###,##0.00");
-            System.out.println(df.format(verbosity));
-<<<<<<< HEAD
-            
-=======
->>>>>>> 6d618da331ac011388fba7804d172b52f6c309f4
+
+
 
         }
 
         catch(FileNotFoundException e){
             e.printStackTrace();
         }
-
+        return verbosity;
 
     }
 }
