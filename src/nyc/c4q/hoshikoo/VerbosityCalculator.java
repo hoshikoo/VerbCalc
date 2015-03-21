484b348b22a01bdd57c8fd8f9a205a40aa794f11
hoshikoo <hoshikooki@gmail.com>
2015-03-21 00:47:08 -0400

    add more code

--- src/nyc/c4q/hoshikoo/VerbosityCalculator.java
+++ src/nyc/c4q/hoshikoo/VerbosityCalculator.java
@@ -3,58 +3,62 @@ package nyc.c4q.ac21;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
+import java.text.DecimalFormat;
 /**
  * Calculate the verbosity of a text
  */
 public class VerbosityCalculator {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         // An abstract representation of a File
         File mobyDick = new File("/Users/Hoshiko/Desktop/accesscode/VerbCalc/resources/MobyDick.txt");
         File aTaleOfTwoCities = new File("/Users/Hoshiko/Desktop/accesscode/VerbCalc/resources/A_Tale_of_Two_Cities.txt");
 
-        try{
-            Scanner sc = new Scanner(mobyDick);
-            Scanner scSecond = new Scanner(aTaleOfTwoCities);
 
-            /**
-             *  Your CODE goes here
+        System.out.println("Hermal Melville's Moby Dick verbosity is ");
+        VerbosityCal(mobyDick);
+        System.out.println("Charles Dickens' A Tale of Two Cities verbosity is ");
+        VerbosityCal(aTaleOfTwoCities);
 
-            System.out.println(sc.next());
-            String text = sc.next();
-            char space = ' ';
-            //String dashes = '--';
-            //System.out.println(dashes);
-            int wordCount=1;
+        if(VerbosityCal(mobyDick)>VerbosityCal(aTaleOfTwoCities)){
+            System.out.println("Hermal Melville is more verbose than Charles Dickens");
+        }else if (VerbosityCal(mobyDick)<VerbosityCal(aTaleOfTwoCities)){
+            System.out.println("Charles Dickens is more verbose than Hermal Melville");
+        }
+    }
 
-            for (int i = 0; i < text.length(); i = i+1){
-                if (text.charAt(i)==space){
-                    wordCount= wordCount+1;
-                }
-            }
-            System.out.println(wordCount);
-             */
+    public static void VerbosityCal(File file) {
+        try {
+
+            Scanner sc = new Scanner(file);
 
 
-            int count =0;
-            int countSecond =0;
+            double wordCount = 0.0;
+            double sentences = 0.0;
 
             while (sc.hasNext()) {
-                ++count;
-                sc.next();
-            }
-            System.out.println(count);
+                ++wordCount;
+                String words = sc.next();
+                //System.out.println(sc.next());
+                if (words.contains(".") || words.contains("!") || words.contains("?"))
+                    sentences=sentences+1;
+                // don't count periods with Mr. and Mrs.
+                if (words.equalsIgnoreCase("mr.") || words.equalsIgnoreCase("mrs.") || words.equalsIgnoreCase("ms."))
+                    sentences=sentences-1;
 
-            while (scSecond.hasNext()) {
-                ++countSecond;
-                scSecond.next();
             }
-            System.out.println(countSecond);
+            double verbosity = wordCount/sentences;
+            //System.out.println(wordCount);
+            //System.out.println(sentences);
+            DecimalFormat df = new DecimalFormat("###,##0.00");
+            System.out.println(df.format(verbosity));
+
         }
 
-        catch (FileNotFoundException e){
+        catch(FileNotFoundException e){
             e.printStackTrace();
         }
+
+
     }
 }
